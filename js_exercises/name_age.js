5e8c3880a5f6d3fe3580a4943ec7a7ca7ad04239
AdamPanag <adampanag06@gmail.com>
2020-05-08 02:26:29 +0300

    Add js folder with name_age exercise

---
+++ js_exercises/name_age.js
@@ -0,0 +1,9 @@
+
+var name = prompt("What is your name?");
+var age = prompt("What is your age?");
+
+var ageInDays = age *365;
+
+alert("Hello " + name + ", you are " + age + " years old.\n" 
+	+ "You are alive for " + ageInDays + " days.");
+console.log(name + " " + age);
